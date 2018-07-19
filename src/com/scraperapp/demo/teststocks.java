08fa94601c6ce32fe9d27acc396fee9490b0b6fc
Anu Tomy <anuatomy@gmail.com>
2018-07-18 21:24:56 -0400

    Display table data vertically

--- src/com/scraperapp/demo/teststocks.java
+++ src/com/scraperapp/demo/teststocks.java
@@ -1,5 +1,7 @@
 package com.scraperapp.demo;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -22,17 +24,36 @@ public class teststocks {
 		//String xpath = "//*[@id=\"scr-res-table\"]/div[2]/table/tbody/tr[*]";
 		String rows = "//*[@id=\"scr-res-table\"]/div[2]/table/tbody/tr[*]";
 		List<WebElement> tableRows = driver.findElements(By.xpath(rows));
-		System.out.println("No. of Rows: " + tableRows.size());
+		int TotalRows = tableRows.size();
+		System.out.println("No. of Rows: " + TotalRows);
 		
 		String colmns = "//*[@id=\"scr-res-table\"]/div[1]/table/thead/tr/th[*]/span/span";
 		List<WebElement> tableColumns = driver.findElements(By.xpath(colmns));
-		System.out.println("No. of Columns: " + tableColumns.size());	
-				
-		for(int i = 0; i < 25; i++) {	
-			
-			System.out.println((i + 1) + " - " + tableRows.get(i).getText());
-		}
+		int TotalColumns = tableColumns.size();
 		
+		System.out.println("No. of Columns: " + TotalColumns);	
+		
+		
+		//Looping through the table to get data horizontally(row wise) 
+//		for(int i = 0; i < TotalRows; i++) {
+//			
+//			System.out.println((i + 1) + " - " + tableRows.get(i).getText());
+//			
+//		}	
+		
+		//Looping through the table to get all data vertically(in one column)
+		for(int i = 0; i < TotalRows; i++) {
+			//To locate columns(cells) of that specific row 
+			List <WebElement> cells = tableRows.get(i).findElements(By.tagName("td"));
+			
+			for(int j = 0; j < TotalColumns; j++) {
+				//To retrieve text from a specific cell
+				String celltext = cells.get(j).getText();
+				System.out.println(celltext);
+			}
+			System.out.println("------------------------");
+		}	
+					
 		driver.close();
 		
 		//wait
@@ -42,3 +63,5 @@ public class teststocks {
 	}
 
 }
+
+
