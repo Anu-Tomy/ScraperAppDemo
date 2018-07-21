73b8ab8773c3688b4ceb8fd397d70255c8547916
Anu Tomy <anuatomy@gmail.com>
2018-07-21 13:33:25 -0400

    Update

--- src/com/scraperapp/demo/StocksTable.java
+++ src/com/scraperapp/demo/StocksTable.java
@@ -6,12 +6,11 @@ import java.sql.PreparedStatement;
 
 public class StocksTable {
 	public static Connection getConnection() throws Exception {
-	    String driver = "com.mysql.jdbc.Driver";
 	    String url = "jdbc:mysql://localhost:3306/yahoo_finance_scraper?useSSL=false";
 	    String username = "yahoo";
 	    String password = "yahoo";
-	    Class.forName(driver);
 	    Connection conn = DriverManager.getConnection(url, username, password);
+	    System.out.println("Database connection successful!");
 	    return conn;
 	  }
 
